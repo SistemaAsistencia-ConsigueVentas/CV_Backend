5b95cc6d42064486415fcc4f819e23c0721a3743
CarlosFarfan29 <cfarfana@unac.edu.pe>
2023-10-25 11:45:59 -0500

    fix: Schedule Service

--- backendv2/app/Http/Controllers/ScheduleController.php
+++ backendv2/app/Http/Controllers/ScheduleController.php
@@ -4,16 +4,25 @@ namespace App\Http\Controllers;
 
 use App\Models\Attendance;
 use App\Models\Schedule;
+use App\Services\ScheduleService;
 use DateTime;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 class ScheduleController extends Controller
 {
+    protected $scheduleService;
+
+    public function __construct(ScheduleService $scheduleService)
+    {
+        $this->scheduleService = $scheduleService;
+    }
+
     public function getSchedules()
     {
         try {
-            return Schedule::get();
+            $schedules = $this->scheduleService->getAllSchedules();
+            return response()->json(['message' => 'Horarios obtenidos exitosamente.', 'data' => $schedules], 200);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error al obtener los horarios.'], 500);
         }
@@ -22,9 +31,7 @@ class ScheduleController extends Controller
     public function createSchedule(Request $request)
     {
         try {
-            $authUser = auth()->id();
-            $request['user_id'] = $authUser; 
-            $schedule = Schedule::create($request->all());
+            $schedule = $this->scheduleService->createSchedule($request->all());
             return response()->json(['message' => 'Horario creado exitosamente.', 'data' => $schedule], 201);
         } catch (\Exception $e) {
             return response()->json(['message' => 'Error al crear el horario.'], 500);
@@ -35,22 +42,11 @@ class ScheduleController extends Controller
     {
         try {
             $authUser = auth()->user();
-            print($currentTime->format('w'));
-            // Obtener el horario del usuario para el día actual
-            $schedule = Schedule::where('day_of_week', $currentTime->format('w'))->where('user_id', $authUser->id)->first();
-            if (!$schedule) {
-                // No tiene horario definido para el día actual
-                return 'Sin horario';
-            }
-            if ($currentTime < $schedule->start_time) {
-                return 'A tiempo';
-            } elseif ($currentTime > $schedule->start_time && $currentTime < $schedule->end_time) {
-                return 'Llegó tarde';
-            } elseif ($currentTime > $schedule->end_time) {
-                return 'Salió temprano';
-            } else {
-                return 'Hora correcta de salida';
-            }
+
+            // Llamamos al servicio para comprobar la asistencia
+            $result = $this->scheduleService->checkAttendance($authUser, $currentTime);
+            return response()->json(['message' => $result]);
+
         } catch (ModelNotFoundException $e) {
             return response()->json(['message' => 'Horario no encontrado.'], 404);
         } catch (\Exception $e) {
--- backendv2/app/Services/ScheduleService.php
+++ backendv2/app/Services/ScheduleService.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace App\Services;
 
+use App\Models\Schedule;
 use App\Repositories\ScheduleRepositories\ScheduleRepositoryInterface;
+use DateTime;
 
 class ScheduleService
 {
@@ -15,5 +17,40 @@ class ScheduleService
         $this->scheduleRepository = $scheduleRepository;
     }
 
-    
+    public function getAllSchedules()
+    {
+        return $this->scheduleRepository->all();
+    }
+
+    public function createSchedule(array $data)
+    {
+        $authUser = auth()->id();
+        $request['user_id'] = $authUser; 
+        return $this->scheduleRepository->create($data);
+    }
+
+    public function checkAttendance($user, DateTime $currentTime)
+    {
+        
+        // Obtener el día de la semana actual
+        $dayOfWeek = $currentTime->format('w');
+
+        // Obtener el horario del usuario para el día actual
+        $schedule = Schedule::where('day_of_week', $dayOfWeek)
+                            ->where('user_id', $user->id)
+                            ->first();
+        if (!$schedule) {
+            // No tiene horario definido para el día actual
+            return 'Sin horario';
+        }
+        if ($currentTime < $schedule->start_time) {
+            return 'A tiempo';
+        } elseif ($currentTime > $schedule->start_time && $currentTime < $schedule->end_time) {
+            return 'Llegó tarde';
+        } elseif ($currentTime > $schedule->end_time) {
+            return 'Salió temprano';
+        } else {
+            return 'Hora correcta de salida';
+        }
+    }
 }
