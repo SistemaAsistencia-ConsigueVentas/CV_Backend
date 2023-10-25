6946070015e59f91ad0dec9c56e740f3a9000b62
CarlosFarfan29 <cjfarfana@unac.edu.pe>
2023-10-25 09:50:05 -0500

    add: ScheduleService

---
+++ backendv2/app/Repositories/ScheduleRepositories/ScheduleRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repositories\ScheduleRepositories;
+
+use App\Models\Schedule;
+
+interface ScheduleRepositoryInterface {
+    public function all(): iterable;
+    public function find(int $id): ?Schedule;
+    public function create(array $data): Schedule;
+    public function update(int $id, array $data): bool;
+    public function delete(int $id): bool;
+}
