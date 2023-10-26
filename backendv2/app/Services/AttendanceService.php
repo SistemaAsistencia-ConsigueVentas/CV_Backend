f0032ed5ede20d237bb7e85805e462b315fe459f
Caleb <reateguicevallosc@gmail.com>
2023-10-26 09:31:30 -0500

    Update: Schedule Service

--- backendv2/app/Services/AttendanceService.php
+++ backendv2/app/Services/AttendanceService.php
@@ -45,7 +45,7 @@ class AttendanceService {
     }
 
     private function isLateForCheckIn($checkInTime, $startTime) {
-        return $startTime > $checkInTime; // Devolver true si el usuario llegó tarde
+        return $checkInTime > $startTime; // Devolver true si el usuario llegó tarde
     }
 
     private function uploadImage($image) {
@@ -112,31 +112,35 @@ class AttendanceService {
             $user_schedule = Schedule::where('user_id', $authUser)
                                         ->where('day_of_week', $dayOfWeek)
                                         ->first();
-    
-            // Actualizar los datos de asistencia
-            $attendance->admission_time = $currentTime->format('H:i');
-            $attendance->admission_image = $this->uploadImage($imagePath);
-            $attendance->user_id = $authUser;
-            $attendance->date = $currentTime->format('Y-m-d');
-    
-            // Verificar si el usuario llegó tarde según el horario personalizado
-            if ($this->isLateForCheckIn($attendance->admission_time, $user_schedule->start_time)) {
-                // El usuario llegó tarde
-                $type = $this->hasJustification();
-    
-                if ($type == 2) {
-                    $attendance->delay = 1;
-                } else {
-                    $attendance->justification = 1;
-                    $attendance->delay = 1;
-                }
-            } else {
-                // El usuario llegó a tiempo
-                $attendance->attendance = 1;
-            }
-    
-            $attendance->save();
             
+            if ($user_schedule) {
+                // Actualizar los datos de asistencia
+                $attendance->admission_time = $currentTime->format('H:i');
+                $attendance->admission_image = $this->uploadImage($imagePath);
+                $attendance->user_id = $authUser;
+                $attendance->date = $currentTime->format('Y-m-d');
+        
+                // Verificar si el usuario llegó tarde según el horario personalizado
+                if ($this->isLateForCheckIn($attendance->admission_time, $user_schedule->start_time)) {
+                    // El usuario llegó tarde
+                    $type = $this->hasJustification();
+                    if ($type == 2) {
+                        $attendance->delay = 1;
+                    } else {
+                        $attendance->justification = 1;
+                        $attendance->delay = 1;
+                    }
+                } else {
+                    // El usuario llegó a tiempo
+                    $attendance->attendance = 1;
+                }
+
+                $attendance->save();
+
+            } else {
+                throw new \Exception('No existe un horario para el usuario elegido', 500);
+            }
+
         } catch (\Exception $e) {
             throw new \Exception('Error al actualizar el check-in.', 500);
         }
