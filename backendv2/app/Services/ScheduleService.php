8e45e72aa9b4019bc6a380c2a3595b1c067aa5ce
Oscar Fernando Muñoz Gayoso <86798585+OscarMG4@users.noreply.github.com>
2023-10-26 09:46:53 -0500

    Update: updateAndDeleteSchedule

--- backendv2/app/Services/ScheduleService.php
+++ backendv2/app/Services/ScheduleService.php
@@ -39,4 +39,38 @@ class ScheduleService
             return $this->scheduleRepository->create($data);
         }
     }
+
+    public function updateSchedule(int $id, array $data)
+    {
+        try {
+            $schedule = $this->scheduleRepository->find($id);
+            if ($schedule) {
+                $updated = $this->scheduleRepository->update($id, $data);
+                if ($updated) {
+                    return response()->json(['message' => 'Horario actualizado exitosamente.'], 200);
+                }
+                throw new \Exception('Error al actualizar el horario.');
+            }
+            throw new \Exception('Horario no encontrado.');
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
+
+    public function deleteSchedule(int $id)
+    {
+        try {
+            $schedule = $this->scheduleRepository->find($id);
+            if ($schedule) {
+                $deleted = $this->scheduleRepository->delete($id);
+                if ($deleted) {
+                    return response()->json(['message' => 'Horario eliminado exitosamente.'], 200);
+                }
+                throw new \Exception('Error al eliminar el horario.');
+            }
+            throw new \Exception('Horario no encontrado.');
+        } catch (\Exception $e) {
+            return response()->json(['message' => $e->getMessage()], 500);
+        }
+    }
 }
